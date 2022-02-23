4e0fc53787f539378e0fcc88ca8cf76e70e006e2
jongkeun-kim <kjkandrea@gmail.com>
2022-02-23 19:30:17 +0900

    Update sample data fetch function

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,4 @@
 import axios from 'axios';
 
-export const getRepositories = () =>
-  axios.get('https://api.github.com/repos/kjkandrea/why-react-query').then(res => res.data);
+export const getIssues = () =>
+  axios.get('https://api.github.com/repos/kjkandrea/why-react-query/issues').then(res => res.data);
