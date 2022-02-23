3e7281f6ff503485e1df6bef54dd4375c5d49776
jongkeun-kim <kjkandrea@gmail.com>
2022-02-23 10:41:23 +0900

    Add react-query playground

---
+++ src/components/todos/Todos.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import {} from 'react-query';
+
+const Todos = () => <h1>안녕 나 투두리스트</h1>;
+
+export default Todos;
--- src/views/TodoApp.tsx
+++ src/views/TodoApp.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import Todos from '@/components/todos/Todos';
 
 const queryClient = new QueryClient();
 
 const TodoApp = () => (
   <QueryClientProvider client={queryClient}>
-    <h1>안녕, 이건 리액트 쿼리를 테스트 해보는 투두앱이야!!</h1>
+    <Todos />
   </QueryClientProvider>
 );
 
