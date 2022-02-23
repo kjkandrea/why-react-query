0473bfa15122dc0abb76d99dc95fb565631d6bce
jongkeun-kim <kjkandrea@gmail.com>
2022-02-23 10:41:35 +0900

    Remove legacy

--- src/api/index.ts
+++
@@ -1,6 +0,0 @@
-import { create } from '@shopby/shop-sdk';
-
-export const api = create(process.env.API_URL as string, {
-  clientId: 'ebbIhzCAKLJHYg+Ks9V45g==',
-  'Content-type': 'application/json',
-});
