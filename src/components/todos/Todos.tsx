cb4dcbed07555c2993a350cf757d39ba23908388
jongkeun-kim <kjkandrea@gmail.com>
2022-02-26 14:01:50 +0900

    Add fetch issues

--- src/components/todos/Todos.tsx
+++ src/components/todos/Todos.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect } from 'react';
-import { getRepositories } from '@/api';
+import { getIssues } from '@/api';
 // import {} from 'react-query';
 
 const Todos = () => {
   useEffect(() => {
-    getRepositories().then(console.log);
+    getIssues().then(console.log);
   }, []);
 
   return <h1>안녕 나 투두리스트</h1>;
