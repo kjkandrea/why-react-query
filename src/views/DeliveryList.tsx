4c8f48d6c1084cb1e094977e653faa40a5f71458
jongkeun-kim <kjkandrea@gmail.com>
2022-03-01 12:31:32 +0900

    Add orderedProductByDeliveryNoGroupSquareArray
    이제 복잡도 증가 그만

--- src/views/DeliveryList.tsx
+++ src/views/DeliveryList.tsx
@@ -1,10 +1,11 @@
 import React, { useMemo } from 'react';
-import { getDeliveryByOrderNos, getOrders } from '../../fixture/order/mockAPI';
+import { getDeliveryByOrderNos, getOrderedProductByOrderNo, getOrders } from '../../fixture/order/mockAPI';
 import { useQuery } from 'react-query';
-import { Delivery, Order } from '../../fixture/interface';
+import { Delivery, Order, OrderedProduct } from '../../fixture/interface';
 
 interface OrderByDeliveryNo extends Delivery {
   orderNo: Order['orderNo'];
+  orderProducts: OrderedProduct[];
 }
 
 // TODO: 이 복잡한걸 어떻게 은닉한다??
@@ -21,14 +22,25 @@ const DeliveryList = () => {
     },
   );
 
-  const orderByDeliveryNoGroupSquareArray = useMemo(() => {
+  const { data: orderedProducts } = useQuery(
+    ['orderedProducts', orderNos],
+    () => orderNos && getOrderedProductByOrderNo(orderNos),
+    {
+      enabled: Array.isArray(orderNos) && orderNos.length > 0,
+    },
+  );
+
+  const orderedProductByDeliveryNoGroupSquareArray = useMemo(() => {
     if (deliveriesByOrderNo === undefined) return;
+    if (orderedProducts === undefined) return;
     const orderByDeliveryNoHashTable = deliveriesByOrderNo.reduce<{
       [key: string]: OrderByDeliveryNo[];
     }>((deliveryHashTable, delivery) => {
       const matchedOrder = orders?.find(order => order.orderNo === delivery.orderNo);
       if (matchedOrder === undefined) return deliveryHashTable;
-      const orderByDeliveryNo = Object.assign(matchedOrder, delivery);
+      const orderByDeliveryNo = Object.assign(matchedOrder, delivery, {
+        orderProducts: orderedProducts.filter(orderedProduct => orderedProduct.orderNo === delivery.orderNo),
+      });
 
       deliveryHashTable[orderByDeliveryNo.deliveryNo]
         ? deliveryHashTable[orderByDeliveryNo.deliveryNo].push(orderByDeliveryNo)
@@ -36,11 +48,40 @@ const DeliveryList = () => {
       return deliveryHashTable;
     }, {});
     return Object.values(orderByDeliveryNoHashTable);
-  }, [deliveriesByOrderNo]);
+  }, [deliveriesByOrderNo, orderedProducts]);
 
-  console.log(orderByDeliveryNoGroupSquareArray);
+  console.log(orderedProductByDeliveryNoGroupSquareArray);
 
-  return <h1>응애 나 딜리버리 리스트</h1>;
+  return orderedProductByDeliveryNoGroupSquareArray === undefined ? (
+    <p className={'data__empty'}>데이터가 없습니다.</p>
+  ) : (
+    <ul>
+      {orderedProductByDeliveryNoGroupSquareArray.map(square => {
+        const representOrder = square[0];
+        return (
+          <li key={representOrder.deliveryNo}>
+            <h2>배송번호 : {representOrder.deliveryNo}</h2>
+            <p>배송지 : {representOrder.deliveryAddress}</p>
+            <ul>
+              {square.map(order => (
+                <li key={order.orderNo}>
+                  <h3>주문 번호{order.orderNo}</h3>
+                  <ul>
+                    {order.orderProducts.map(product => (
+                      <li key={product.productNo}>
+                        <h4>{product.productName}</h4>
+                        <p>판매가 : {product.salePrice.toLocaleString('en-us')}</p>
+                      </li>
+                    ))}
+                  </ul>
+                </li>
+              ))}
+            </ul>
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
 
 export default DeliveryList;
